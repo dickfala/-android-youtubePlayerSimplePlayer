d31da97c4ea787b06d7ee74c86045dfdd8fecacc
Yuanyu Chang 張原瑜 (中光電) <yuanyu.chang@coretronic.com>
2014-10-17 15:06:08 +0800

    init

---
+++ src/com/example/YoutubeAPIDemo/MyActivity.java
@@ -0,0 +1,49 @@
+package com.example.YoutubeAPIDemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.security.Provider;
+
+public class MyActivity extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener{
+
+    // enter google api key
+    private final  String API_KEY = "enter google api key";
+    // enter video id
+    private final  String VIDEO_ID = "enter video id";
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        YouTubePlayerView youTubePlayerView = (YouTubePlayerView)findViewById(R.id.youtubeplayerview);
+        youTubePlayerView.initialize(API_KEY, this);
+
+    }
+
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        if (!b) {
+            youTubePlayer.cueVideo(VIDEO_ID);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+
+        Toast.makeText(getApplicationContext(),
+                "onInitializationFailure()",
+                Toast.LENGTH_LONG).show();
+    }
+}
